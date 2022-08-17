ee44a3ad734f487e676512fe04b11945754b89e6
Jamshid <jamshid@nadcab.com>
2022-08-17 12:32:16 +0530

    changes done

---
+++ src/Wrapper.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Body from './Body';
+import Sidebar from './Sidebar';
+
+const Wrapper = () => {
+    return (
+        <div className='indx'>
+            
+            <Sidebar/>
+            <Body/>
+            
+        </div>
+    );
+};
+
+export default Wrapper;
